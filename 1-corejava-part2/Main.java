c1fcafd4e797547028b9d579d499151abe40d042
Captain Storm <CaptainStorm21@users.noreply.github.com>
2024-04-23 00:00:37 -0700

    Added a new subsection to Java 1 core

---
+++ 1-corejava-part2/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+      System.out.println("Java Object Oriented Programming");
+      System.out.println();
+
+
+      System.out.println("Class and Object Practical");
+      ConversionType.main(args);
+      System.out.println();
+
+    }
+  }
+
+
